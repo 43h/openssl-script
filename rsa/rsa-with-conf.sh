43adfd31eb1c7ee5eaa49aaf0e10c75f5efa2d6e
43h <cw.clx@outlook.com>
2024-03-28 18:42:07 +0800

    Update rsa-with-conf.sh

--- rsa/rsa-with-conf.sh
+++ rsa/rsa-with-conf.sh
@@ -1,17 +1,24 @@
-##
-#使用特定配置文件，生成特定作用的证书
-##
-#! /bin/sh
-./lc.sh
 
-mkdir demoCA
-echo 01 > demoCA/serial
-touch demoCA/index.txt
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 domain name"
+    echo "example: $0 baidu.com"
+    exit 1
+fi
 
-# CA self certificate
-openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:2048 -out ca-cert.pem -keyout ca.key -subj /C=CN/ST=GD/L=SZ/O=TEST/OU=NULL/CN="TEST EV Root CA" -config ../conf/ca.conf
+cp ../conf/web.conf ./web.conf
+if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+	echo "Parameter is an IP address"
+    sed -i 's/^DNS.*//g' web.conf
+	sed -i "s/^IP\.1.*/IP\.1 = $1/g" web.conf
+else
+	echo "Parameter is a domain name"
+	sed -i "s/^DNS\.1.*/DNS\.1 = $1/g" web.conf
+	sed -i 's/^IP.*//g' web.conf
+fi
 
 #issue a certificate for the web server
-openssl genrsa -out peer1.key 2048
-openssl req -new -batch -out peer1.csr -key peer1.key -subj /CN=peer1.cn/C=CN/ST=GD/L=SZ/O=TEST/OU=NULL -batch -config ../conf/web.conf
-openssl ca -cert ca-cert.pem -keyfile ca.key -in peer1.csr -out peer1-cert.pem -outdir . -batch -config ../conf/web.conf 
+openssl genrsa -out $1.key 2048
+openssl req -new -batch -out $1.csr -key $1.key -subj /CN=$1/C=CN/ST=GD/L=SZ/O=TEST/OU=NULL -batch -config web.conf
+openssl ca -cert ca-cert.pem -keyfile ca.key -in $1.csr -out $1-cert.pem -outdir . -batch -config web.conf 
+rm -rf $1.csr
+rm -rf web.conf
